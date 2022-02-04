5d7c346f2a6d92e55bd9cad7060072ff9498fdab
sjjung-june <78462290+sjjung-june@users.noreply.github.com>
2022-02-04 14:19:24 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+import requests
+
+indeed_result = requests.get("https://kr.indeed.com/%EC%B7%A8%EC%97%85?as_and=python&as_phr&as_any&as_not&as_ttl&as_cmp&jt=all&st&salary&radius=25&l&fromage=any&limit=50&sort&psf=advsrch&from=advancedsearch&vjk=882e071610c0a8cd")
+
+print(indeed_result)
